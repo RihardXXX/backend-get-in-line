0f0d369145771a54ceceef4c55cae213e08447f7
RihardXXX <yusupa.akaeva@yandex.ru>
2024-03-03 00:07:52 +0300

    feat(index): connect BD mongo

---
+++ src/bd_settings/index.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { Mongoose } from 'mongoose';
+import dotenv from 'dotenv';
+import * as process from "process";
+// Загрузка переменных окружения из файла .env
+dotenv.config();
+
+const mongo_db_path = process.env.MONGO_DB_PATH
+const mongo_db_database = process.env.MONGO_DB_DATABASE
+
+export async function startMongo():Promise<Mongoose> {
+    try {
+        const connection = await mongoose.connect(`${mongo_db_path}/${mongo_db_database}`)
+        console.log('MongoDB connected')
+        return connection
+    } catch (e) {
+        console.error('MongoDB connection error:', e)
+        throw e;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import dotenv from 'dotenv';
-
-import { greeting } from '@src/utils';
-
-console.log(112, greeting);
+import * as process from "process";
+import { startMongo } from "@src/bd_settings";
 
 // Загрузка переменных окружения из файла .env
 dotenv.config();
 
 const app = express();
+const domain = process.env.DOMAIN || 'http://localhost'
 const port = process.env.PORT || 3050;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
 });
 
-app.listen(port, () => {
-    console.log(`Server start на http://localhost:${port}`);
-});
+// старт сервера поэтапно после запуска БД
+async function startServer():Promise<any> {
+    // запуск подключения к БД
+    await startMongo()
+    // запуск express
+    app.listen(port, () => console.log(`Server start на ${domain}:${port}`));
+}
+
+// запуск сервера
+(async () => {
+    await startServer()
+})()
